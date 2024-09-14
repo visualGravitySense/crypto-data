79ae3c45a1affc38fc4ef6d0ca743253f8a961b9
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-09-14 20:42:52 +0300

    converter component & retro design

--- src/SearchResult.jsx
+++ src/SearchResult.jsx
@@ -1,16 +1,17 @@
 import Table from 'react-bootstrap/Table';
+import './SearchResult.css';
 
 function SearchResult() {
   return (
-    <div>
+    <div className="mt-5 search-result-container">
         <h3>Search Result</h3>
         <Table striped bordered hover>
         <thead>
             <tr>
             <th>#</th>
-            <th>First Name</th>
-            <th>Last Name</th>
-            <th>Username</th>
+            <th>Raiting</th>
+            <th>Badge</th>
+            <th>Name</th>
             </tr>
         </thead>
         <tbody>
