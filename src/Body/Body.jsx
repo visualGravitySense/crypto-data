8b2e0b4d44f337cd0b35c9897919441acac75a3e
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-09-19 21:55:58 +0300

    popUp for chart

--- src/Body/Body.jsx
+++ src/Body/Body.jsx
@@ -1,11 +1,13 @@
 import GlobalData from './GlobalData';
 import ListCoins from './ListCoins';
+import ChartModal from './CoinPage/ChartModal';
 
 function Body() {
     return (
         <>
             <GlobalData />
             <ListCoins />
+            <ChartModal />
         </>
     );
 }
