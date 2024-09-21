5f141d8a8609b8b695c74f4a6a802e3e89119f5e
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-09-21 21:09:50 +0300

    new modal window with child and scss

--- src/Body/CoinPage/ChildModal.jsx
+++ src/Body/CoinPage/ChildModal.jsx
@@ -2,23 +2,17 @@ import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
-function ChildModal() {
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+function ChildModal({ show, handleClose, children }) {
 
   return (
-    <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
-      </Button>
-
-      <Modal show={show} onHide={handleClose}>
+    <>      
+      <Modal size="xl" show={show} onHide={handleClose}>
         <Modal.Header closeButton>
           <Modal.Title>Modal heading</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Body>
+            {children}
+        </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
--- src/Body/CoinPage/CoinPage.jsx
+++ src/Body/CoinPage/CoinPage.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import CoinPriceSection from "./CoinPriceSection";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
@@ -5,8 +6,15 @@ import CoinMetrics from "./CoinMetrics";
 import CoinChart from "./CoinChart";
 import ChartPeriods from "./ChartPeriods";
 import Button from 'react-bootstrap/Button'
+import ChildModal from './ChildModal'
 
 function CoinPage() {
+  const [childModalShow, setChildModalShow] = React.useState(false);
+
+  const handleShow = () => setChildModalShow(true);
+  const handleClose = () => setChildModalShow(false);
+  
+
   return (
     <>
       <CoinPriceSection />
@@ -14,13 +22,20 @@ function CoinPage() {
         <Col md={4}>
           <CoinMetrics />
         </Col>
-        <Col md={8}>
-          <ChartPeriods />
-          <CoinChart />
-          <Button variant="primary">Zoom</Button>
-          
+        <Col md={8}>                              
         </Col>
       </Row>
+
+      <Button onClick={handleShow} variant="primary">
+        Zoom
+      </Button>
+
+      <ChildModal show={childModalShow} handleClose={handleClose}>
+        
+        <CoinChart />
+        <ChartPeriods />
+        
+      </ChildModal>
     </>
   );
 }
