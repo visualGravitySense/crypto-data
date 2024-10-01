44b6332458bbc0e781da8ea409f9be9686f1f10d
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-10-01 20:25:45 +0300

    publish on github

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
+    <BrowserRouter basename="/crypto-data">
         <App />
     </BrowserRouter>
 
