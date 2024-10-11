f7ae866e91bdcb9eb96ea8376bbed169da29a83c
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-10-11 11:34:20 +0300

    hero component ready

--- src/Body/GlobalData.jsx
+++ src/Body/GlobalData.jsx
@@ -7,8 +7,7 @@ import { getGlobalData } from "../services/api";
 import ErrorModal from "./ErrorModal";
 import { BodyContext } from "../providers/BodyProvider";
 
-
-import './hero.scss'; 
+import "./hero.scss";
 
 function GlobalData() {
   const [globalData, setGlobalData] = React.useState({});
@@ -23,36 +22,57 @@ function GlobalData() {
         setErrorMessage(
           "Coin List is not available. Error: " + error.toString()
         )
-      )
-      // .finally(() => setIsLoading(false));
+      );
+    // .finally(() => setIsLoading(false));
   }, []);
 
   return (
     <>
-
-<section className="hero-section">
-  <Row>
-      <div className="hero-container">
-        {/* Основная секция, которая занимает 2/3 ширины */}
-        <div className="hero-content">
-          <h1>
-          Crypto community and
-            <span className="highlighted-word">Knowledge </span>sharing 
-          </h1>
-          {/* <p className="responsive-text">
+      <section className="hero-section">
+        <Row>
+          <div className="hero-container">
+            {/* Основная секция, которая занимает 2/3 ширины */}
+            <div className="hero-content">
+              <h1>
+                Crypto community and
+                <span className="highlighted-word">Knowledge </span>sharing
+              </h1>
+              {/* <p className="responsive-text">
           Join educators who are already creating courses, teaching students, and earning from their expertise. 
           </p> */}
-          <div className="new-block">
-            <p className="new-block-text">
-            Our platform offers everything you need to learn about cryptocurrency. Don't miss the chance to be part of a growing community!            </p>
-            <button className="btn-primary">Get Started</button>
-          </div>
-        </div>
+              <div className="new-block">
+                <p className="new-block-text">
+                  Our platform offers everything you need to learn about
+                  cryptocurrency. Don't miss the chance to be part of a growing
+                  community!
+                </p>
 
-        {/* Вторая секция, которая занимает 1/3 ширины */}
-        <div className="hero-image">
+                <div className="global-data">
 
-        <Table >
+                <div>
+                BTC Dominance:<p className="highlighted-item">{globalData.bitcoin_dominance_percentage} %</p>
+                </div>
+
+                <div>
+                Vol 24h:<p className="highlighted-item">{globalData.volume_24h_usd}</p>
+                </div>
+
+                <div>
+                Market Cap:<p className="highlighted-item">{globalData.market_cap_usd}</p>
+                </div>
+
+
+                </div>
+
+                
+              </div>
+
+              <button className="btn-primary">Get Started</button>
+            </div>
+
+            {/* Вторая секция, которая занимает 1/3 ширины */}
+            <div className="hero-image">
+              {/* <Table >
         <tbody>
           <tr>
             <td>BTC Dominance</td>
@@ -72,33 +92,27 @@ function GlobalData() {
           </tr>
         </tbody>
         
-      </Table>
+      </Table> */}
 
-          {/* Вы можете добавить сюда изображение или любой другой контент */}
-          {/* <img src="https://via.placeholder.com/400" alt="Placeholder Image" /> */}
-          {/* <img src="cryptoImage" alt="Hero" /> */}
-          {/* <img
+              {/* Вы можете добавить сюда изображение или любой другой контент */}
+              {/* <img src="https://via.placeholder.com/400" alt="Placeholder Image" /> */}
+              {/* <img src="cryptoImage" alt="Hero" /> */}
+              {/* <img
             src={cryptoImage}
             alt="About Our Platform"
             className="about-image"
           /> */}
-
-      
-        </div>
-
-        
-      </div>
-      </Row>
-      <ErrorModal
-        errorMessage={errorMessage}
-        show={!!errorMessage}
-        handleClose={() => setErrorMessage(null)}
-      />
-    </section>
-
+            </div>
+          </div>
+        </Row>
+        <ErrorModal
+          errorMessage={errorMessage}
+          show={!!errorMessage}
+          handleClose={() => setErrorMessage(null)}
+        />
+      </section>
     </>
   );
 }
 
 export default GlobalData;
-
