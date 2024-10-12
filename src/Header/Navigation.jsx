44145e1a06af42f471b1d2e56f253b115701ba6b
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-10-12 16:30:57 +0300

    footer design,

--- src/Header/Navigation.jsx
+++ src/Header/Navigation.jsx
@@ -9,6 +9,86 @@ import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setSelectedCurrency } from "../services/store";
 
+import styled from 'styled-components';
+// import { Navbar, Nav, NavDropdown, Form, Button, Container } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
+
+// Стилизованные компоненты анти-дизайна
+const StyledNavbar = styled(Navbar)`
+  background-color: #ff006680;
+  border-bottom: 5px solid #ff006680;
+  font-family: 'Courier New', Courier, monospace;
+  padding: 0.9rem 1rem;
+  box-shadow: 2px 2px 0px #ff006680;
+  transition: all 0.3s ease;
+  
+  .navbar-brand {
+    font-size: 2rem;
+    font-weight: bold;
+    color: #000;
+    text-shadow: 3px 3px 0px #ff0066;
+    &:hover {
+      color: #000;
+      text-shadow: none;
+    }
+  }
+
+  .nav-link {
+    color: #000;
+    font-size: 1.25rem;
+    font-weight: bold;
+    margin: 0 1rem;
+    text-transform: uppercase;
+    text-shadow: 2px 2px 0px #ff0066;
+    transition: transform 0.3s ease;
+    &:hover {
+      color: #fff;
+      transform: translate(-3px, -3px);
+      // text-shadow: none;
+      text-shadow: 2px 2px 0px #ff0066;
+    }
+  }
+
+  .navbar-toggler {
+    background-color: #fff;
+    border: 2px solid #000;
+    padding: 0.5rem;
+  }
+
+  .navbar-collapse {
+    justify-content: space-between;
+  }
+`;
+
+const StyledForm = styled(Form)`
+  input {
+    border: 3px solid #000;
+    padding: 0.5rem;
+    background-color: #fff;
+    color: #000;
+    box-shadow: 1px 1px 0px #000;
+    &:focus {
+      outline: none;
+      box-shadow: 6px 6px 0px #000;
+    }
+  }
+
+  button {
+    background-color: #000;
+    color: #fff;
+    border: 3px solid #ffcc00;
+    padding: 0.5rem 1rem;
+    font-weight: bold;
+    transition: all 0.3s ease;
+    box-shadow: 5px 5px 0px #ff0066;
+    &:hover {
+      background-color: #ffcc00;
+      color: #000;
+      box-shadow: 2px 2px 0px #000;
+    }
+  }
+`;
+
 function Navigation() {
   const navigate = useNavigate();
 
@@ -31,19 +111,16 @@ function Navigation() {
   };
 
   return (
-    <Navbar expand="lg" className="bg-body-tertiary mb-4">
+    <StyledNavbar expand="lg">
       <Container fluid>
+      <Link to="/" className="nav-link">
         <Navbar.Brand>Bison Wise</Navbar.Brand>
+      </Link>
+        
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
+          <Nav className="me-auto">
             <Link to="/" className="nav-link">Home</Link>
-            {/* <Nav.Link href="#action1">Home</Nav.Link> */}
-            {/* <Nav.Link href="#action2">Link</Nav.Link> */}
             <NavDropdown title="Currency" id="navbarScrollingDropdown">
               {currencies.map((currency) => (
                 <NavDropdown.Item
@@ -55,29 +132,67 @@ function Navigation() {
                 </NavDropdown.Item>
               ))}
             </NavDropdown>
-            <Link to="/exchange" className="nav-link">
-            Exchanges
-            </Link>
-            <Link to="/compare" className="nav-link">
-            Compare
-            </Link>
-            {/* <Nav.Link href="#" disabled>
-              Link
-            </Nav.Link> */}
+            <Link to="/exchange" className="nav-link">Exchanges</Link>
+            <Link to="/compare" className="nav-link">Compare</Link>
           </Nav>
-          <Form className="d-flex" onSubmit={handleSubmit}>
+          <StyledForm className="d-flex" onSubmit={handleSubmit}>
             <Form.Control
               type="search"
               placeholder="Search"
-              className="me-2"
-              aria-label="Search"
               name="q"
             />
-            <Button type="submit" variant="outline-success">Search</Button>
-          </Form>
+            <Button type="submit">Search</Button>
+          </StyledForm>
         </Navbar.Collapse>
       </Container>
-    </Navbar>
+    </StyledNavbar>
+    // <Navbar expand="lg" className="bg-body-tertiary mb-4">
+    //   <Container fluid>
+    //     <Navbar.Brand >Bison Wise</Navbar.Brand>
+    //     <Navbar.Toggle aria-controls="navbarScroll" />
+    //     <Navbar.Collapse id="navbarScroll">
+    //       <Nav
+    //         className="me-auto my-2 my-lg-0"
+    //         style={{ maxHeight: "100px" }}
+    //         navbarScroll
+    //       >
+    //         <Link to="/" className="nav-link">Home</Link>
+    //         {/* <Nav.Link href="#action1">Home</Nav.Link> */}
+    //         {/* <Nav.Link href="#action2">Link</Nav.Link> */}
+    //         <NavDropdown title="Currency" id="navbarScrollingDropdown">
+    //           {currencies.map((currency) => (
+    //             <NavDropdown.Item
+    //               active={selectedCurrency.name === currency.name}
+    //               key={currency.name}
+    //               onClick={() => dispatch(setSelectedCurrency(currency))}
+    //             >
+    //               {currency.name} {currency.symbol}
+    //             </NavDropdown.Item>
+    //           ))}
+    //         </NavDropdown>
+    //         <Link to="/exchange" className="nav-link">
+    //         Exchanges
+    //         </Link>
+    //         <Link to="/compare" className="nav-link">
+    //         Compare
+    //         </Link>
+    //         {/* <Nav.Link href="#" disabled>
+    //           Link
+    //         </Nav.Link> */}
+    //       </Nav>
+    //       <Form className="d-flex" onSubmit={handleSubmit}>
+    //         <Form.Control
+    //           type="search"
+    //           placeholder="Search"
+    //           className="me-2"
+    //           aria-label="Search"
+    //           name="q"
+    //         />
+    //         <Button type="submit" variant="outline-success">Search</Button>
+    //       </Form>
+    //     </Navbar.Collapse>
+    //   </Container>
+    // </Navbar>
   );
 }
 
