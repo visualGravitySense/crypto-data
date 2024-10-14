19eb299d3b809234220d6de8de8ecec9dcd6b5ff
Dmitri Gornakov <dmitri.gornakov@gmail.com>
2024-10-14 20:28:56 +0300

    exchange list

--- src/Body/ExchangeList.jsx
+++ src/Body/ExchangeList.jsx
@@ -2,27 +2,40 @@ import React from "react";
 import Table from "react-bootstrap/Table";
 import { BodyContext } from "../providers/BodyProvider";
 
+import './ExchangeList.scss';  // Подключаем SCSS файл
+
+
 function ExchangeList() {
   console.log(ExchangeList);
 
   const { exchangeList } = React.useContext(BodyContext);
   return (
-    <Table>
+    <Table className="table-compare">
       <thead>
         <tr>
           <th>Name</th>
+          <th>Rank</th>
+          <th>Reported Volume 24h Share (%)</th>
+          <th>Number of Markets</th>
+          <th>Country</th>
+          <th>Trust Score</th>
+          <th>Year Established</th>
+          <th>Trading Incentives</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>
-            {exchangeList.slice(0, 100).map((exchange) => (
-              <tr key={exchange.id}>
-                <td>{exchange.name}</td>
-              </tr>
-            ))}
-          </td>
-        </tr>
+        {exchangeList.slice(0, 100).map((exchange) => (
+          <tr key={exchange.id}>
+            <td>{exchange.name}</td>
+            <td>{exchange.adjusted_rank}</td>
+            <td>{exchange.reported_volume_24h_share}%</td>
+            <td>{exchange.market_count}</td>
+            <td>{exchange.country || 'N/A'}</td>
+            <td>{exchange.trust_score || 'N/A'}</td>
+            <td>{exchange.year_established || 'N/A'}</td>
+            <td>{exchange.has_trading_incentive ? 'Yes' : 'No'}</td>
+          </tr>
+        ))}
       </tbody>
     </Table>
   );
